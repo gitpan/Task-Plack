1979f0ef8cbaf7c8a14c014a3ca2db053bd3e2c2
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2009-11-07 05:06:59 +0000

    Import of MIYAGAWA/Task-Plack-0.11 from CPAN.
    gitpan-cpan-distribution: Task-Plack
    gitpan-cpan-version:      0.11
    gitpan-cpan-path:         MIYAGAWA/Task-Plack-0.11.tar.gz
    gitpan-cpan-author:       MIYAGAWA
    gitpan-cpan-maturity:     released

--- lib/Task/Plack.pm
+++ lib/Task/Plack.pm
@@ -2,7 +2,7 @@ package Task::Plack;
 
 use strict;
 use 5.008_001;
-our $VERSION = '0.10';
+our $VERSION = '0.11';
 use ExtUtils::MakeMaker;
 
 use base qw(Exporter);
@@ -42,7 +42,7 @@ sub dependencies {
             [ undef, 'Sledge::PSGI', 'git://github.com/mala/Sledge-PSGI.git' ],
         ],
         'CGI::Application::PSGI', sub { has_module('CGI::Application') }, [
-            [ undef, 'CGI::Application::PSGI', 'git://github.com/miyagawa/CGI-Application-PSGI.git' ],
+            [ 'CGI::Application::PSGI', 'git://github.com/markstos/CGI-Application-PSGI.git' ],
         ],
         'Maypole::PSGI', sub { has_module('Maypole') }, [
             [ undef, 'Maypole::PSGI', 'git://github.com/miyagawa/Maypole-PSGI.git' ],
